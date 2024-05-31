566ffd7e74d21d976eaf9ae822470af97ef6ae7d
Carlos Leal <carlos.frleal@gmail.com>
2024-05-31 19:30:01 -0300

    feat: instalação do Tailwind e componente header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Switch>
+        <Route>
+          <Header />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="flex bg-slate-800">
+      <h1>Header</h1>
+      <img src={ '' } alt="company logo" id="header-logo" />    
+    </div>
+  )
+}
+
+export default Header;
