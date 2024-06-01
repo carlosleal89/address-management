f197363dc56932875a80d7abef0e370461f26677
Carlos Leal <carlos.frleal@gmail.com>
2024-06-01 12:51:52 -0300

    feat: styles card mars

--- src/components/marsAddress/MarsAdressCard.tsx
+++ src/components/marsAddress/MarsAdressCard.tsx
@@ -7,9 +7,13 @@ import BoldText from '../textTags/BoldText';
 export const MarsAdressCard: React.FC<addressProps> = ({ addressData }) => {
   return (
     <>
-      <img src={marsImg} alt='imagem de marte' className='w-10' />
+      <img src={marsImg} alt='imagem de marte' className='w-10 mt-1 mb-auto' />
       <BoldText label='Lote:' text={addressData.address} />
-      <Button text='Editar' route={`/edit-address/${addressData.id}`} />
+      <Button
+        text='Editar'
+        route={`/edit-address/${addressData.id}`}
+        classes='sm:w-10 h-10 my-auto ml-auto mr-1 justify-center'
+      />
     </>
   )
 }
